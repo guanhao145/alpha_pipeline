63d2264bc41324fa09cf6e4d9440b43db611a6ff
Guan Qiao <gq959070@lvnvdc0469.lvn.broadcom.net>
2021-07-07 21:50:46 -0700

    unpacked array assignment test

--- study/array_assignment_packed.v
+++ study/array_assignment_packed.v
@@ -7,17 +7,24 @@ logic [1:0][1:0][7:0] packed_3d_array;
 //   packed_3d_array <= '0;      // assign 0 to all elements of array
 // end
   initial begin
-	packed_3d_array = '0;      // assign 0 to all elements of array
-    packed_3d_array[0][0][0]   = 1'b0;         // assign one bit
-    packed_3d_array[0][0]      = 8'h0a;        // assign one element
-    packed_3d_array[0][0][3:0] = 4'ha;         // assign part select
-    packed_3d_array[0]         = 16'habcd;     // assign slice
-    packed_3d_array            = 32'h01234567; // assign entire array as vector
+  	$display("packed_3d_array = %h", packed_3d_array);
+	packed_3d_array = 0;      // assign 0 to all elements of array
+	// packed_3d_array = '0;	// what is the different.
+  	$display("packed_3d_array = %h", packed_3d_array);
 
+    packed_3d_array[0][0][0]   = 1'b0;         // assign one bit
   	$display("packed_3d_array[0][0][0] = %h", packed_3d_array[0][0][0]);
+
+    packed_3d_array[0][0]      = 8'h0a;        // assign one element
   	$display("packed_3d_array[0][0] = %h", packed_3d_array[0][0]);
+
+	packed_3d_array[0][0][3:0] = 4'ha;         // assign part select
   	$display("packed_3d_array[0][0][3:0] = %h", packed_3d_array[0][0][3:0]);
+
+    packed_3d_array[0]         = 16'habcd;     // assign slice
   	$display("packed_3d_array[0] = %h", packed_3d_array[0]);
+
+    packed_3d_array            = 32'h01234567; // assign entire array as vector
   	$display("packed_3d_array = %h", packed_3d_array);
   end
 endmodule
--- study/array_assignment_unpacked.v
+++ study/array_assignment_unpacked.v
@@ -7,23 +7,23 @@ logic [7:0] e_array[3:0]; // note index of unpacked dimension is reversed
 logic [7:0] mult_array_a[3:0][3:0];
 logic [7:0] mult_array_b[3:0][3:0];
 
-always_ff @(posedge clk, negedge rst_n)
-  if (!rst_n) begin
-    d_array <= '{default:0};      // assign 0 to all elements of array
-  end
-  else begin
-    d_array        <= '{8'h00, c, b, a}; // d_array[0]=8'h00, d_array[1]=c, d_array[2]=b, d_array[3]=a
-    e_array        <= '{8'h00, c, b, a}; // e_array[3]=8'h00, e_array[2]=c, e_array[1]=b, d_array[0]=a
-    mult_array_a   <= '{'{8'h00, 8'h01, 8'h02, 8'h03},
+
+  initial begin
+  	$display("d_array = %p", d_array);
+    d_array 		= '{default:0};      // assign 0 to all elements of array
+  	$display("d_array = %p", d_array);
+    d_array        = '{8'h00, 8'hc, 11, 4'ha}; // d_array[0]=8'h00, d_array[1]=c, d_array[2]=b, d_array[3]=a
+  	$display("d_array = %p", d_array);
+    e_array        = '{8'h00, 4'hc, 11, 4'ha}; // e_array[3]=8'h00, e_array[2]=c, e_array[1]=b, d_array[0]=a
+    mult_array_a   = '{'{8'h00, 8'h01, 8'h02, 8'h03},
                         '{8'h04, 8'h05, 8'h06, 8'h07},
                         '{8'h08, 8'h09, 8'h0a, 8'h0b},
                         '{8'h0c, 8'h0d, 8'h0e, 8'h0f}}; // assign to full array
-    mult_array_b[3] <= '{8'h00, 8'h01, 8'h02, 8'h03}; // assign to slice of array
-  end
-  $display("d_array = %p", d_array);
+    mult_array_b[3] = '{8'h00, 8'h01, 8'h02, 8'h03}; // assign to slice of array
   $display("e_array = %p", e_array);
   $display("mult_array_a = %p", mult_array_a);
   $display("mult_array_b[3] = %p", mult_array_b[3]);
+  end
 endmodule
 
 // reference
